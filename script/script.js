f4cb22dc4402df0192e0e0c4e5f3d7b908184005
pagos21 <paga@localhost.yeah>
2020-07-06 17:17:51 +0200

    v 0.2

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
 console.log("Here!");
+$(document).ready(function(){
+  $(".p_drop").mouseenter(
+    function(){
+      $(this).next(".dropCont").fadeIn("slow");
+    }
+  )
+
+
+  $(".p_drop").mouseleave(
+    function(){
+      $(this).next(".dropCont").fadeOut("slow");
+    }
+  )
+})
