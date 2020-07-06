9ca37c05e96b739bb58480391b5bc6b5b9e22a92
pagos21 <paga@localhost.yeah>
2020-07-06 17:26:37 +0200

    v 0.2.1

--- script/script.js
+++ script/script.js
@@ -2,14 +2,14 @@ console.log("Here!");
 $(document).ready(function(){
   $(".p_drop").mouseenter(
     function(){
-      $(this).next(".dropCont").fadeIn("slow");
+      $(this).next(".dropCont").fadeIn("fast");
     }
   )
 
 
   $(".p_drop").mouseleave(
     function(){
-      $(this).next(".dropCont").fadeOut("slow");
+      $(this).next(".dropCont").fadeOut("fast");
     }
   )
 })
